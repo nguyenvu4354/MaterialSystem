711ae7aa56fa09c6febb6589c69d14886fa128da
DESKTOP-MAC88L0\nguye <nguyenvu150203@gmail.com>
2025-05-27 13:45:28 +0700

    Test

--- MaterialManagement/src/java/controller/UserListServlet.java
+++ MaterialManagement/src/java/controller/UserListServlet.java
@@ -20,7 +20,7 @@ public class UserListServlet extends HttpServlet {
 protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
 
-    request.setCharacterEncoding("UTF-8"); // nếu cần
+    request.setCharacterEncoding("UTF-8"); 
 
     int page = 1;
     int pageSize = 5;
