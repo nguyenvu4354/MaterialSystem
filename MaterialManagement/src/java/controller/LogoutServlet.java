5f8770a217ffbc245db22ab074e4a215306725d5
DESKTOP-MAC88L0\nguye <nguyenvu150203@gmail.com>
2025-05-30 12:13:29 +0700

    Save local changes before pulling

--- MaterialManagement/src/java/controller/LogoutServlet.java
+++ MaterialManagement/src/java/controller/LogoutServlet.java
@@ -14,12 +14,10 @@ public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        HttpSession session = request.getSession(false); // false = không tạo mới nếu không tồn tại
+        HttpSession session = request.getSession(false); 
         if (session != null) {
             session.invalidate();
         }
-
-        // Chuyển hướng về trang đăng nhập
         response.sendRedirect("Login.jsp");
     }
 
