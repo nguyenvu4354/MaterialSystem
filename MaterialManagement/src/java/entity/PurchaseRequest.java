aa64717fb1c3d72dcdc5e3f67cdff50ac4810770
AnNguyenHE181270 <annthe181270@fpt.edu.vn>
2025-06-10 20:05:32 +0700

    5.1 + 6.1 + 7.1 Purchase Request

---
+++ MaterialManagement/src/java/entity/PurchaseRequest.java
@@ -0,0 +1,165 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author Admin
+ */
+
+import java.util.Date;
+
+public class PurchaseRequest {
+     private int id;
+    private String requestCode;
+    private int userId;
+    private Date requestDate;
+    private String status;
+    private Double estimatedPrice;
+    private String reason;
+    private Integer approvedBy;
+    private String approvalReason;
+    private Date approvedAt;
+    private String rejectionReason;
+    private Date createdAt;
+    private Date updatedAt;
+    private boolean disable;
+
+    public PurchaseRequest() {
+    }
+
+    
+    
+    public PurchaseRequest(int id, String requestCode, int userId, Date requestDate, String status, Double estimatedPrice, String reason, Integer approvedBy, String approvalReason, Date approvedAt, String rejectionReason, Date createdAt, Date updatedAt, boolean disable) {
+        this.id = id;
+        this.requestCode = requestCode;
+        this.userId = userId;
+        this.requestDate = requestDate;
+        this.status = status;
+        this.estimatedPrice = estimatedPrice;
+        this.reason = reason;
+        this.approvedBy = approvedBy;
+        this.approvalReason = approvalReason;
+        this.approvedAt = approvedAt;
+        this.rejectionReason = rejectionReason;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.disable = disable;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRequestCode() {
+        return requestCode;
+    }
+
+    public void setRequestCode(String requestCode) {
+        this.requestCode = requestCode;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Double getEstimatedPrice() {
+        return estimatedPrice;
+    }
+
+    public void setEstimatedPrice(Double estimatedPrice) {
+        this.estimatedPrice = estimatedPrice;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public Integer getApprovedBy() {
+        return approvedBy;
+    }
+
+    public void setApprovedBy(Integer approvedBy) {
+        this.approvedBy = approvedBy;
+    }
+
+    public String getApprovalReason() {
+        return approvalReason;
+    }
+
+    public void setApprovalReason(String approvalReason) {
+        this.approvalReason = approvalReason;
+    }
+
+    public Date getApprovedAt() {
+        return approvedAt;
+    }
+
+    public void setApprovedAt(Date approvedAt) {
+        this.approvedAt = approvedAt;
+    }
+
+    public String getRejectionReason() {
+        return rejectionReason;
+    }
+
+    public void setRejectionReason(String rejectionReason) {
+        this.rejectionReason = rejectionReason;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public boolean isDisable() {
+        return disable;
+    }
+
+    public void setDisable(boolean disable) {
+        this.disable = disable;
+    }
+    
+    
+}
---
+++ MaterialManagement/web/PurchaseRequestForm.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : PurchaseRequestForm
+    Created on : Jun 10, 2025, 7:20:31 PM
+    Author     : Admin
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
